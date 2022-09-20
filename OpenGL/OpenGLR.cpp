5cf75780edcc49b5da3e12de292e954ab06d8ae3
H7perus <jonathanott.gd@gmail.com>
2022-09-21 01:50:07 +0200

    Starting point for flight sim

--- OpenGL/OpenGLR.cpp
+++ OpenGL/OpenGLR.cpp
@@ -1,5 +1,8 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 
 #include <glm/glm/glm.hpp>
 #include <glm/glm/gtc/matrix_transform.hpp>
@@ -10,6 +13,7 @@
 #include <iostream>
 #include <string>
 #include "shader.h"
+#include "model.h"
 
 #include "stb_image.h"
 
@@ -18,11 +22,19 @@ glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 
@@ -39,47 +51,48 @@ int main()
 	-0.5f,  0.5f, 0.0f, 0.5, 0.5, 0.0,  0.0f, 1.0f
 	};*/
 	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		// positions          // normals           // texture coords
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
 
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
 
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
 
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
 
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
 
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
 	};
 
 	glm::vec3 cubePositions[] = {
@@ -101,7 +114,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGLR", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1600, 900, "OpenGLR", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -110,6 +123,8 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -117,9 +132,11 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1600, 900);
 
-	Shader testShader("vertexShader.vert", "fragmentShader.frag");
+	Shader testShader("vertexShader.vert", "debugShader.frag");
+	Shader testShader2("vertexShader.vert", "fragmentShader.frag");
+	Shader lightShader("vertexShader.vert", "light_shader.frag");
 
 
 
@@ -129,65 +146,19 @@ int main()
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
 
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	unsigned int texture0, texture1;
-	glGenTextures(1, &texture0);
-	glBindTexture(GL_TEXTURE_2D, texture0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	testShader.setFloat("horizontalOffset", 0.5f);
-
-
-
-
-
-	stbi_set_flip_vertically_on_load(true);
-
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-
-	stbi_image_free(data);
-
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
 	testShader.use();
-	testShader.setInt("albedo0", 0);
-	testShader.setInt("albedo1", 1);
+	/*testShader.setInt("material.diffuse", 0);
+	testShader.setInt("material.specular", 1);*/
 
 	//transform matrix
 	//glm::mat4 trans = glm::mat4(1.0f);
@@ -202,7 +173,7 @@ int main()
 
 
 	glm::mat4 projection;
-	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	projection = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 100.0f);
 
 
 	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -211,15 +182,19 @@ int main()
 	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
 
-	const float radius = 10.0f;
-	float camX = sin(glfwGetTime()) * radius;
-	float camZ = cos(glfwGetTime()) * radius;
+	float radius = 2.0f;
+	glm::vec3 lights[2];
 
 	glm::mat4 view;
 
 	glfwSwapInterval(0);
 
 	glEnable(GL_DEPTH_TEST);
+
+	Model ourModel("../assets/Fw190A5.obj");
+	Model floor("../assets/floor.obj");
+	
+	std::cout << ourModel.meshes.size() << std::endl;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -231,40 +206,87 @@ int main()
 		glm::mat4 trans = glm::mat4(1.0f);
 		trans = glm::translate(trans, glm::vec3(0.0f, -0.0f, 0.0f));
 		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-		
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-
+		testShader.use();
 		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
 		glClearColor(0.2f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture1);
+		testShader.setMat4("model", model);
+		testShader.setMat4("projection", projection);
+		testShader.setMat4("view", view);
+		
 
-		testShader.use();
-		unsigned int modelLoc = glGetUniformLocation(testShader.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		unsigned int viewLoc = glGetUniformLocation(testShader.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		unsigned int projectionLoc = glGetUniformLocation(testShader.ID, "projection");
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
-		
-		for (unsigned int i = 0; i < 10; i++)
+
+		lightShader.use();
+
+		lightShader.setMat4("view", view);
+		lightShader.setMat4("projection", projection);
+		for (int i = 0; i < 2; i++)
 		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0, 0.3, 0.4f));
-			testShader.setMat4("model", model);
+			lights[i] = glm::vec3(sin(glfwGetTime() * 0.5 + i) * radius, 0, cos(glfwGetTime() * 0.5 + i) * radius);
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), lights[i]);
+			model = glm::scale(model, glm::vec3(0.2));
+
+			lightShader.setMat4("model", model);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
+		testShader.use();
+		testShader.setVec3("pointLights[0].position", lights[0]);
+		testShader.setVec3("pointLights[0].ambient", glm::vec3(0.5, 0.5, 0.5));
+		testShader.setVec3("pointLights[0].diffuse", glm::vec3(0.5, 0.5, 0.5));
+		testShader.setVec3("pointLights[0].specular", glm::vec3(0.8, 0.8, 0.8));
+		testShader.setVec3("pointLights[1].position", lights[1]);
+		testShader.setVec3("pointLights[1].ambient", glm::vec3(0.5, 0.5, 0.5));
+		testShader.setVec3("pointLights[1].diffuse", glm::vec3(0.8, 0.8, 0.8));
+		testShader.setVec3("pointLights[1].specular", glm::vec3(0.8, 0.8, 0.8));
+		testShader.setVec3("sun.ambient", glm::vec3(0.1, 0.1, 0.1));
+		testShader.setVec3("sun.diffuse", glm::vec3(0.5, 0.48, 0.45));
+		testShader.setVec3("sun.specular", glm::vec3(0.5, 0.48, 0.45));
+		testShader.setVec3("sun.direction", glm::vec3(1.0, 1.0, 1.0));
+		
+		testShader.setVec3("viewPos", cameraPos);
+		testShader.setFloat("material.shininess", 64.0f);
+		//for (unsigned int i = 0; i < 10; i++)
+		//{
+		//	glm::mat4 model = glm::mat4(1.0f);
+		//	model = glm::translate(model, cubePositions[i]);
+		//	float angle = 20.0f * i;
+		//	model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0, 0.3, 0.4f));
+		//	testShader.setMat4("model", model);
+		//	ourModel.Draw(testShader);
+		//	//glDrawArrays(GL_TRIANGLES, 0, 36);
+		//}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		testShader.setMat4("model", model);
+		ourModel.Draw(testShader);
+
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		testShader2.use();
+		testShader2.setVec3("pointLights[0].position", lights[0]);
+		testShader2.setVec3("pointLights[0].ambient", glm::vec3(0.5, 0.5, 0.5));
+		testShader2.setVec3("pointLights[0].diffuse", glm::vec3(0.5, 0.5, 0.5));
+		testShader2.setVec3("pointLights[0].specular", glm::vec3(0.8, 0.8, 0.8));
+		testShader2.setVec3("pointLights[1].position", lights[1]);
+		testShader2.setVec3("pointLights[1].ambient", glm::vec3(0.5, 0.5, 0.5));
+		testShader2.setVec3("pointLights[1].diffuse", glm::vec3(0.8, 0.8, 0.8));
+		testShader2.setVec3("pointLights[1].specular", glm::vec3(1.5, 1.8, 1.8));
+
+		testShader2.setVec3("sun.ambient", glm::vec3(0.5, 0.5, 0.5));
+		testShader2.setVec3("sun.diffuse", glm::vec3(0.8, 0.8, 0.8));
+		testShader2.setVec3("sun.specular", glm::vec3(1.5, 1.8, 1.8));
+		testShader2.setVec3("sun.direction", glm::vec3(1.0, 1.0, 1.0));
+
+		testShader2.setVec3("viewPos", cameraPos);
+		testShader2.setFloat("material.shininess", 128.0f);
+		testShader2.setMat4("model", model);
+		testShader2.setMat4("projection", projection);
+		testShader2.setMat4("view", view);
+		floor.Draw(testShader2);
 
 
 
@@ -287,7 +309,42 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
 
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
--- OpenGL/fragmentShader.frag
+++ OpenGL/fragmentShader.frag
@@ -1,13 +1,128 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 customColor;
-in vec2 TexCoord;
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
 
-uniform sampler2D albedo0;
-uniform sampler2D albedo1;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
+struct Material {
+	vec3 ambient;
+	sampler2D diffuse;
+	sampler2D specular;
+	float shininess;
+};
+struct pointLight {
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+struct dirLight {
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+struct spotLight {
+	vec3 position;
+	vec3 direction;
+	float cone;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Material material;
+
+#define NR_POINT_LIGHTS 2  
+uniform pointLight pointLights[NR_POINT_LIGHTS];
+uniform dirLight sun;
+
+
+
+vec3 calcPointLight(pointLight light)
+{
+	//ambient lighting
+	vec3 ambient = vec3(texture(texture_diffuse1, TexCoords)) * light.ambient;
+
+	//specular lighting
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(light.position - FragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = (diff * vec3(texture(texture_diffuse1, TexCoords))) * light.diffuse;
+
+	// specular
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = vec3(texture(texture_specular1, TexCoords)) * spec * light.specular;
+	//return vec3(1.0);
+	return (diffuse + specular + ambient) * (1 / ( 1 + pow((distance(FragPos, light.position)), 2)));
+}
+vec3 calcSpotLight(spotLight light)
+{
+	float theta = dot(normalize(light.position - FragPos), normalize(light.direction));
+	vec3 ambient = vec3(0.0);
+	vec3 diffuse = vec3(0.0);
+	vec3 specular = vec3(0.0);
+	if(theta > light.cone)
+	{
+	//ambient lighting
+		ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
+		//specular lighting
+		vec3 norm = normalize(Normal);
+		vec3 lightDir = normalize(light.position - FragPos);
+		float diff = max(dot(norm, lightDir), 0.0);
+		diffuse = (diff * vec3(texture(material.diffuse, TexCoords))) * light.diffuse;
+
+	// specular
+		vec3 viewDir = normalize(viewPos - FragPos);
+		vec3 reflectDir = reflect(-lightDir, norm);
+		float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+		specular = vec3(texture(material.specular, TexCoords)) * spec * light.specular;
+	}
+	return (ambient + diffuse + specular) * (1 / ( 1 + pow((distance(FragPos, light.position)), 2)));
+}
+vec3 calcDirLight(dirLight light)
+{
+		//ambient lighting
+	vec3 ambient = light.ambient * vec3(texture(texture_diffuse1, TexCoords));
+
+	//specular lighting
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(light.direction);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = (diff) * light.diffuse * vec3(texture(texture_diffuse1, TexCoords));
+
+	// specular
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = spec * light.specular * vec3(texture(texture_specular1, TexCoords));
+	return (ambient + diffuse + specular);
+}
+
 
 void main()
 {
-   FragColor = mix(texture(albedo0, TexCoord), texture(albedo1, TexCoord), 0.2);
+	vec3 result;
+	for(int i = 0; i < NR_POINT_LIGHTS; i++)
+	{
+		result += calcPointLight(pointLights[i]);
+	}
+	result += calcDirLight(sun);
+	 
+	FragColor = vec4(result, 1.0);
+	//FragColor = vec4(1.0);
+	//FragColor = mix(texture(albedo, TexCoords), texture(specular, TexCoords), 0.5);
+	//FragColor = vec4(ambient, 0);
 }
